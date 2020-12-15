74bab05dccff8aebfa55367e9ea8daa0d053dff2
JvanKaam <65725956+JvanKaam@users.noreply.github.com>
2020-12-15 15:52:35 +0100

    commit

---
+++ script.js
@@ -0,0 +1,46 @@
+
+
+const addTheWordCool = function(array){
+    array.push("cool").toString();
+}
+
+console.log("Add cool:", addTheWordCool(["nice", "awesome", "tof"]));
+
+
+
+
+
+//
+/*
+let arrayAantal = ['appels', 'peren', 'citroenen'];
+
+const amountOfElementsInArray = function(array) {
+    console.log(array.length);
+}
+
+console.log(amountOfElementsInArray(arrayAantal));
+
+//
+
+let arrayFirstRetour = ["Belgie", "Nederland", "Luxemburg"];
+
+const selectBelgiumFromBenelux = function(array) {
+    arrayFirstRetour[0];
+}
+
+console.log(selectBelgiumFromBenelux([arrayFirstRetour])); 
+
+
+
+
+
+
+
+
+function myFunction() {
+    let ary=arrayFirstRetour[0];
+}
+
+console.log(myFunction[arrayFirstRetour]);
+
+*/
